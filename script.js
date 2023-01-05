d6a4a30e5e8b819f71e827c3baf5311b87297643
Adama Saho <adama.webdev@gmail.com>
2023-01-05 01:09:46 +0100

    Done

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ let listArrays = [];
 
 // Drag Functionality
 let draggedItem;
+let dragging = false;
 let currentColumn;
 
 
@@ -117,10 +118,14 @@ function updateDOM() {
 function updateItem(id, column){
   const selectedArray = listArrays[column];
   const selectedColumnEl = listColumns[column].children;
-  if(!selectedColumnEl[id].textContent){
-    delete selectedArray[id];
+  if(!dragging){
+    if(!selectedColumnEl[id].textContent){
+      delete selectedArray[id];
+    }else{
+      selectedArray[id] = selectedColumnEl[id].textContent
+    }
+    updateDOM();
   }
-  updateDOM();
 }
 
 //Ad to Column List, Reset Textbox
@@ -172,6 +177,7 @@ function rebuildArrays(){
 //When Item gets dragged
 function drag(e){
   draggedItem = e.target;
+  dragging = true;
 }
 //Column Allows for Item to drop
 function allowDrop(e){
@@ -194,6 +200,8 @@ function drop(e){
   //Add Item to Column
   const parent = listColumns[currentColumn];
   parent.appendChild(draggedItem);
+  //Draging complete
+  dragging = false;
   rebuildArrays();
 }
 
